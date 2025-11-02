5259dbb5b071b70c795644f1077ebab13296ca8f
unknown <72mo2v@gmail.com>
2025-11-02 12:20:57 -0800

    update-v25

--- src/pages/Auth/CompanySelection.jsx
+++ src/pages/Auth/CompanySelection.jsx
@@ -3,7 +3,7 @@
 // ======================================
 
 import React from 'react';
-import CompanySelector from '../components/Auth/CompanySelector';
+import CompanySelector from '../../components/Auth/CompanySelector';
 
 const CompanySelectionPage = () => {
   return <CompanySelector />;
